b235acfe8dc4646460a758637d0f3f242c7bceec
d-lau <d-lau@mercari.com>
2020-08-09 00:18:56 +0900

    rearrange to do work in parse, handle errors

--- parse.go
+++ parse.go
@@ -15,30 +15,22 @@ const (
 	ERROR_STRING         = "Error: "
 )
 
-func Parse(input io.Reader) []*ResourcePlan {
-	// Overall:
-	// Look for the start of resources
-	// No changes -> return
-	// Changes -> start parsing
-	// New / Force Replace -> Parse every line
-	// Update in place -> parse only changed lines
-	// Destroy -> name only
-	return []*ResourcePlan{}
-}
-
+// Overall:
+// Look for the start of resources
+// No changes -> return
+// Changes -> start parsing
+// New / Force Replace -> Parse every line
+// Update in place -> parse only changed lines
+// Destroy -> name only
 // TODO: handle multi level structs
-func ParseFromFile(filepath string) []*ResourcePlan {
-	f, err := os.Open(filepath)
-	if err != nil {
-		panic(err)
-	}
-
+func Parse(input io.Reader) ([]*ResourcePlan, error) {
 	result := []*ResourcePlan{}
 	var resourcePlan *ResourcePlan
 	var mapAttributeChange *MapAttributeChange
+	var err error
 
 	parse := false
-	scanner := bufio.NewScanner(f)
+	scanner := bufio.NewScanner(input)
 	for scanner.Scan() {
 		text := strings.TrimSpace(uncolor(scanner.Bytes()))
 
@@ -51,7 +43,7 @@ func ParseFromFile(filepath string) []*ResourcePlan {
 				if resourcePlan != nil {
 					result = append(result, resourcePlan)
 				}
-				return result
+				return result, nil
 			}
 
 			if IsResourceCommentLine(text) {
@@ -61,18 +53,18 @@ func ParseFromFile(filepath string) []*ResourcePlan {
 
 				resourcePlan, err = NewResourcePlanFromComment(text)
 				if err != nil {
-					panic(err)
+					return result, err
 				}
 			} else if IsMapAttributeChangeLine(text) {
 				mapAttributeChange, err = NewMapAttributeChangeFromLine(text)
 				if err != nil {
-					panic(err)
+					return result, err
 				}
 			} else if IsAttributeChangeLine(text) {
 				log.Printf("running for line %v\n", text)
 				ac, err := NewAttributeChangeFromLine(text)
 				if err != nil {
-					panic(err)
+					return result, err
 				}
 				if mapAttributeChange != nil {
 					mapAttributeChange.AttributeChanges = append(mapAttributeChange.AttributeChanges, ac)
@@ -90,7 +82,7 @@ func ParseFromFile(filepath string) []*ResourcePlan {
 		} else {
 			if strings.Contains(text, NO_CHANGES_STRING) || strings.Contains(text, ERROR_STRING) {
 				// Nothing to parse, return empty plan
-				return result
+				return result, nil
 			} else if strings.Contains(text, CHANGES_START_STRING) {
 				// Parse all lines from here on
 				parse = true
@@ -102,5 +94,14 @@ func ParseFromFile(filepath string) []*ResourcePlan {
 		result = append(result, resourcePlan)
 	}
 
-	return result
+	return result, nil
+}
+
+func ParseFromFile(filepath string) ([]*ResourcePlan, error) {
+	f, err := os.Open(filepath)
+	if err != nil {
+		return []*ResourcePlan{}, err
+	}
+
+	return Parse(f)
 }
